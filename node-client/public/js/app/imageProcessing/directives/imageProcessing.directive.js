53c9d7263f8383bb673f257c192f115c3d857efe
dancebutter <502381314@HCU-BS6TH12.logon.ds.ge.com>
2015-08-21 10:51:01 -0700

    load image success

--- node-client/public/js/app/imageProcessing/directives/imageProcessing.directive.js
+++ node-client/public/js/app/imageProcessing/directives/imageProcessing.directive.js
@@ -14,13 +14,49 @@ define(
 
             }
 
-            ImageProcessingController.$inject = [ '$scope' ];
-            function ImageProcessingController( $scope ) {
-                $scope.testStr = "Image Processing Directive Test";
+            ImageProcessingController.$inject = [ '$scope','$log', '$http' ];
+            function ImageProcessingController( $scope, $log, $http ) {
+                /* variable */
+                $scope.imgFile = null;
+
+                /* functions */
+                $scope.imgLoad = imgLoad;
+
+                function imgLoad( element ) {
+                    if( element && element.files.length >=1 ) {
+                        var file = element.files[0];
+                        loadImgData( file, processImgData );
+                    } else {
+                        $log.error("[ERROR] Load image failed, files length invaild: " + element.files.length );
+                    }
+                }
+
+                function loadImgData( file, callback ) {
+                    var reader = new FileReader();
+
+                    reader.onload = function( theFile ) {
+                        $log.debug( "[DEBUG] Load Image Data Success.");
+                        processImgData( theFile );
+                    };
+                    reader.onerror = function( error ) {
+                        $log.error( "[ERROR] Load Image Data failed. " + error );
+                        uploadFailed( error );
+                    };
+                    reader.readAsDataURL( file );
+                }
+
+                function processImgData( event ) {
+                    console.log( event );
+                }
+
+                function uploadFailed( event ) {
+                    console.log( event );
+                }
+
             }
 
             return {
-                restrict: 'A',
+                restrict: 'E',
                 link: ImageProcessingLink,
                 controller: ImageProcessingController,
                 templateUrl: './js/app/imageProcessing/views/imageProcessing.tmpl.html'
